d8c92698312bff8b95f829bf254180dbab4f5d98
Paul-Louis NECH <me@plnech.fr>
2015-07-03 19:39:34 +0200

    Style: Removed ActionBar of Home; Renamed some styles
    Former-commit-id: 0e91a8c00b51749a0b115326ff02787da5a22dc0

--- app/src/main/java/com/ionis/igem/app/ui/HomeActivity.java
+++ app/src/main/java/com/ionis/igem/app/ui/HomeActivity.java
@@ -53,30 +53,6 @@ public class HomeActivity extends AppCompatActivity implements SurfaceHolder.Cal
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    // TODO: Discuss usefulness of OptionsMenu
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void surfaceCreated(SurfaceHolder holder) {
         int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
         ViewGroup.LayoutParams lp = videoView.getLayoutParams();
