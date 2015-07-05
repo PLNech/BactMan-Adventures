f08098a76aa2504e4a07b8b028b0822c3ee2783e
Paul-Louis NECH <me@plnech.fr>
2015-07-05 18:23:13 +0200

    What: Minor changes; added link
    Former-commit-id: 2fa25e5a645b37e2b2b970565214bc488a478c16

--- app/src/main/java/com/ionis/igem/app/ui/WhatActivity.java
+++ app/src/main/java/com/ionis/igem/app/ui/WhatActivity.java
@@ -1,7 +1,12 @@
 package com.ionis.igem.app.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.ionis.igem.app.R;
 
 public class WhatActivity extends AppCompatActivity {
@@ -10,6 +15,14 @@ public class WhatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_what);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.link)
+    protected void onClickLink() {
+        Intent iWebsite = new Intent(Intent.ACTION_VIEW);
+        iWebsite.setData(Uri.parse(getResources().getString(R.string.what_url)));
+        startActivity(iWebsite);
     }
 
 }
