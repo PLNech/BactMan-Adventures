7632cf38b272a95eaae8750d8833ab5e196d9bac
Paul-Louis NECH <me@plnech.fr>
2015-07-07 15:40:57 +0200

    Utils: Created an underlining helper method
    Former-commit-id: 873c076352bb27bab2523eabc41d4a733989fd86

---
+++ app/src/main/java/com/ionis/igem/app/utils/ViewUtils.java
@@ -0,0 +1,28 @@
+package com.ionis.igem.app.utils;
+
+import android.text.SpannableString;
+import android.text.style.StrikethroughSpan;
+import android.text.style.UnderlineSpan;
+import android.text.style.UpdateAppearance;
+import android.widget.TextView;
+
+/**
+ * Created by PLN on 07/07/2015.
+ */
+public class ViewUtils {
+    public static void textUnderline(TextView view) {
+        UnderlineSpan sUnder = new UnderlineSpan();
+        textTransform(view, sUnder);
+    }
+
+    public static void textStrike(TextView view) {
+        StrikethroughSpan sStrike = new StrikethroughSpan();
+        textTransform(view, sStrike);
+    }
+
+    private static void textTransform(TextView view, UpdateAppearance appearance) {
+        SpannableString viewText = new SpannableString(view.getText());
+        viewText.setSpan(appearance, 0, viewText.length(), 0);
+        view.setText(viewText);
+    }
+}
