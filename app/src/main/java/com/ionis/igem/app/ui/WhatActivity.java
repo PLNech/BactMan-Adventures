d3388adff07c6382c006b06a723bff99c84362ce
Paul-Louis NECH <me@plnech.fr>
2015-07-07 15:23:24 +0200

    What: underlined and made clickable link
    Former-commit-id: 43498467049460b05135f4d76d6a341eea19188e

--- app/src/main/java/com/ionis/igem/app/ui/WhatActivity.java
+++ app/src/main/java/com/ionis/igem/app/ui/WhatActivity.java
@@ -4,18 +4,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 import com.ionis.igem.app.R;
 
 public class WhatActivity extends AppCompatActivity {
+
+    @InjectView(R.id.link)
+    protected TextView linkView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_what);
         ButterKnife.inject(this);
+        SpannableString linkText = new SpannableString(linkView.getText());
+        linkText.setSpan(new UnderlineSpan(), 0, linkText.length(), 0);
+        linkView.setText(linkText);
+
     }
 
     @OnClick(R.id.link)
