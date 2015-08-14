20ad30199940ee1c1a959a8a92b26ea4a83eabfe
Paul-Louis NECH <me@plnech.fr>
2015-08-14 16:48:54 +0200

    Project: Integrated AndEngine
    Former-commit-id: d02f029e646e570e55a64cf00da6aa8ba2157d2b

---
+++ app/src/main/java/com/ionis/igem/app/game/BaseGameActivity.java
@@ -0,0 +1,72 @@
+package com.ionis.igem.app.game;
+
+import android.util.Log;
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.camera.SmoothCamera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+/**
+ * Created by PLN on 11/08/2015.
+ */
+public abstract class BaseGameActivity extends SimpleBaseGameActivity {
+
+    private static final String TAG = "BaseGameActivity";
+
+    private Boolean shouldStartEngine = false;
+
+    private static final int CAMERA_WIDTH = 640;
+    private static final int CAMERA_HEIGHT = 480;
+
+    private static final float MAX_VELOCITY_X = 2.0f;
+    private static final float MAX_VELOCITY_Y = 2.0f;
+    private static final float MAX_ZOOM = 3.0f;
+
+    protected Scene gameScene;
+    protected Camera gameCamera;
+
+    @Override
+    public Engine onCreateEngine(EngineOptions pEngineOptions) {
+        Engine engine = new Engine(pEngineOptions);
+        if(shouldStartEngine){
+            Log.v(TAG, "onCreateEngine - Starting engine.");
+            engine.start();
+            shouldStartEngine = !shouldStartEngine;
+        } else {
+            Log.v(TAG, "onCreateEngine - Not starting engine.");
+        }
+        return engine;
+    }
+
+
+
+    @Override
+    public synchronized void onResumeGame() {
+        if(mEngine != null) {
+            super.onResumeGame();
+            shouldStartEngine = true;
+            Log.v(TAG, "onResumeGame - We have an engine: marked as starting.");
+        } else {
+            Log.v(TAG, "onResumeGame - No engine to start.");
+        }
+    }
+
+
+//    @Override
+//    protected Scene onCreateScene() {
+//        this.mEngine.registerUpdateHandler(new FPSLogger());
+
+//        return null;
+//    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        gameCamera = new SmoothCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT, MAX_VELOCITY_X, MAX_VELOCITY_Y, MAX_ZOOM);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), gameCamera);
+    }
+}
