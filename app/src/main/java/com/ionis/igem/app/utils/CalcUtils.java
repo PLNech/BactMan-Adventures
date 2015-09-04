37dd6ba4c373c7c315b6374ea0fb2c582fcde11b
Paul-Louis NECH <me@plnech.fr>
2015-09-04 18:23:25 +0200

    CalcUtils: Stopped logging; Cached random in static member

--- app/src/main/java/com/ionis/igem/app/utils/CalcUtils.java
+++ app/src/main/java/com/ionis/igem/app/utils/CalcUtils.java
@@ -1,17 +1,13 @@
 package com.ionis.igem.app.utils;
 
-import android.util.Log;
-
 import java.util.Random;
 
 /**
  * Created by PLNech on 28/08/2015.
  */
 public class CalcUtils {
-    private static final String TAG = "CalcUtils";
-
     private static Random random;
-    
+
     public static int randomOf(int max) {
         if (random == null) {
             random = new Random();
@@ -19,8 +15,6 @@ public class CalcUtils {
         return randomOf(max, random);
     }
     public static int randomOf(int max, Random random) {
-        final int res = Math.abs(random.nextInt() % max);
-        Log.d(TAG, "randomOf(" + max + ") -> " + res);
-        return res;
+        return Math.abs(random.nextInt() % max);
     }
 }
