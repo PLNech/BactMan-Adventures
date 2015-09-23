e3fb5ec4fa1f66595deb3edfc0117708452c0148
Paul-Louis NECH <me@plnech.fr>
2015-09-23 15:12:08 -0400

    Contact: Refactored activity

--- app/src/main/java/fr/plnech/igem/ui/ContactUsActivity.java
+++ app/src/main/java/fr/plnech/igem/ui/ContactUsActivity.java
@@ -3,35 +3,42 @@ package fr.plnech.igem.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import android.widget.TextView;
-import butterknife.InjectView;
-import fr.plnech.igem.R;
-
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import fr.plnech.igem.R;
 import fr.plnech.igem.ui.model.DetailActivity;
-import fr.plnech.igem.utils.ViewUtils;
+import fr.plnech.igem.utils.DevUtils;
 
 public class ContactUsActivity extends DetailActivity {
-
-    @InjectView(R.id.link)
-    protected TextView linkView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.inject(this);
-        ViewUtils.textUnderline(linkView);
     }
 
-    @OnClick(R.id.link)
+    @OnClick(R.id.icon_email)
     protected void onClickEmail() {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         Uri data = Uri.parse("mailto:"
-                + R.string.contact_us_email_val);
+                + getResources().getString(R.string.contact_us_email_val));
         intent.setData(data);
         startActivity(Intent.createChooser(intent, "Send email"));
+    }
+
+    @OnClick(R.id.icon_facebook)
+    protected void onClickFacebook() {
+        DevUtils.openFacebookLink(getResources().getString(R.string.url_contact_facebook), this);
+    }
+
+    @OnClick(R.id.icon_twitter)
+    protected void onClickTwitter() {
+        DevUtils.openLink(getResources().getString(R.string.url_contact_twitter), this);
+    }
+
+    @OnClick(R.id.icon_youtube)
+    protected void onClickYoutube() {
+        DevUtils.openLink(getResources().getString(R.string.url_contact_youtube), this);
     }
 
     @Override
